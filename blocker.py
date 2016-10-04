3c973c07fe2b44cbb59eb252de2f334963a8130e
Chris Archibald <carchi8py@gmail.com>
2016-10-03 18:40:17 -0700

    Check in of Python files

---
+++ blocker.py
@@ -0,0 +1,13 @@
+import time
+from datetime import datetime as dt
+
+host_path = '/usr/hosts'
+redirect = "127.0.0.1"
+website_list=["www.facebook.com", "facebook.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
+        print ("Working Hours ...")
+    else:
+        print ("Fun Hours ....")
+    time.sleep(5)
