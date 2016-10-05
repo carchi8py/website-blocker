84d35a7a420bbc28a2154f4ba89af7137fe767a3
Chris Archibald <carchi8py@gmail.com>
2016-10-05 16:47:16 -0700

    add and remove lines to the host file

--- blocker.py
+++ blocker.py
@@ -1,13 +1,26 @@
 import time
 from datetime import datetime as dt
 
-host_path = '/usr/hosts'
+host_path = '/etc/hosts'
+host_temp = "hosts"
 redirect = "127.0.0.1"
 website_list=["www.facebook.com", "facebook.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
         print ("Working Hours ...")
+        with open(host_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + ' ' + website + '\n')
     else:
         print ("Fun Hours ....")
+        with open(host_temp, "r+") as file:
+            content = file.readlines()
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
     time.sleep(5)
